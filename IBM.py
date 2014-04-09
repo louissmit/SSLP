1c61e373d2ab29bc659c4b237b34bc4e1c7f5cc2
louissmit <ghr1Co74G9>
2014-04-09 12:23:50 +0200

    perplexity

--- IBM.py
+++ IBM.py
@@ -1,29 +1,39 @@
 
 from translation import Translation
 from collections import Counter
+import numpy as np
 
 def loadCorpus():
 	source = list(open('corpus.nl', 'r'))
 	target = list(open('corpus.en', 'r'))
 	return source, target
 
-def runEM(source, target):
+def runEM(source, target, alpha):
 	t = Translation()
-	count = Translation()
-	total = Counter()
-	for i in xrange(len(source)):
-		e_s = target[i]
-		f_s = source[i]
-		total_s = Counter()
-		for f in f_s:
-			for e in e_s:
-				if f in total_s:
-					total_s[f] += t(f, e)
-		for f in f_s:
-			for e in e_s:
-				new = t.get(f=f, e=e) / total_s[f]
-				count.update(f=f, e=e, val=new)
-				total[e] += new
+	perplexity = 1000
+	while perplexity > alpha:
+		count = Translation()
+		total = Counter()
+		for i in xrange(len(source)):
+			e_s = target[i]
+			f_s = source[i]
+			total_s = Counter()
+			for f in f_s:
+				for e in e_s:
+					if f in total_s:
+						total_s[f] += t.get(f=f, e=e)
+			for f in f_s:
+				for e in e_s:
+					new = t.get(f=f, e=e) / total_s[f]
+					count.update(f=f, e=e, val=new)
+					total[e] += new
 
-	for e in
+		t.normalize(count, total)
+		perpexity = 0
+		for i in xrange(0, len(source)):
+			e_s = target[i]
+			f_s = source[i]
+			perpexity += np.log2(p(e_s, f_s))
+		perplexity *= -1
+		print perpexity
 
