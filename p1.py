a03e67339f4ca9fb6a8a128eb8af967e897e405c
louissmit <ghr1Co74G9>
2014-04-10 16:26:58 +0200

    viterbi test

--- p1.py
+++ p1.py
@@ -5,85 +5,87 @@ from decimal import Context, Decimal
 from IBM import getViterbiAlignment
 
 def loadCorpus():
-    source = list(open('corpus_1000.nl', 'r'))
-    target = list(open('corpus_1000.en', 'r'))
-    return source, target
+	source = list(open('corpus_1000.nl', 'r'))
+	target = list(open('corpus_1000.en', 'r'))
+	return source, target
 
 source, target = loadCorpus()
 
 def corpus(n=None):
-    """ An iterator of pairs """ 
-    n = n or len(source)
-    for i in xrange(n):
-        yield source[i].split(),target[i].split()
+	""" An iterator of pairs """
+	n = n or len(source)
+	for i in xrange(n):
+		yield source[i].split(),target[i].split()
 
 def round_dc(dc, n=0):
-    """ Rounds a dictionary of Counters """
-    for k,v in dc.iteritems():
-        new = Counter()
-        for i,c in v.iteritems():
-            c = round(c, n)
-            if c:
-                new[i] = c
-        dc[k] = new
-    return dc
+	""" Rounds a dictionary of Counters """
+	for k,v in dc.iteritems():
+		new = Counter()
+		for i,c in v.iteritems():
+			c = round(c, n)
+			if c:
+				new[i] = c
+		dc[k] = new
+	return dc
 
-def translate(pairs, stability=1):
-    """ Builds IBM1 translation table, doing EM until the perplexity difference is < `stability` """
-    # doublecounter : f -> e -> val
-    t = {}
-    count = {}
+def translate(pairs, n=20, stability=1):
+	""" Builds IBM1 translation table, doing EM until the perplexity difference is < `stability` """
+	# doublecounter : f -> e -> val
+	t = {}
+	count = {}
 
-    vocf = Counter([b for a,_ in pairs for b in a])
-    voce = Counter([b for _,a in pairs for b in a])
+	vocf = Counter([b for a,_ in pairs for b in a])
+	voce = Counter([b for _,a in pairs for b in a])
 
-    epsilon = 1.0 # float!!!
-#     epsilon = sum([len(x) for x,_ in pairs]) / float(len(pairs))
-    print 'epsilon:', epsilon
+	epsilon = 1.0 # float!!!
+	#     epsilon = sum([len(x) for x,_ in pairs]) / float(len(pairs))
+	print 'epsilon:', epsilon
 
-    # initialize uniform
-    for f in vocf:
-        t[f] = Counter()
-        for e in voce:
-            t[f][e] = 1.0/len(voce)
+	# initialize uniform
+	for f in vocf:
+		t[f] = Counter()
+		for e in voce:
+			t[f][e] = 1.0/len(voce)
 
-    perplexity = float('inf')
-    stable = False
-    while not stable:
-        for f in vocf:
-            count[f] = Counter()
-        total = Counter()
+	perplexity = float('inf')
+	stable = False
+	i = 0
+	while i < n:
+		for f in vocf:
+			count[f] = Counter()
+		total = Counter()
 
-        for (fs, es) in pairs:
-            total_s = Counter()
-            for f in fs:
-                total_s[f] = sum(t[f].values())
-            for f in fs:
-                for e in es:
-                    count[f][e] += t[f][e] / total_s[f]
-                    total[e] += t[f][e] / total_s[f]
+		for (fs, es) in pairs:
+			total_s = Counter()
+			for f in fs:
+				total_s[f] = sum(t[f].values())
+			for f in fs:
+				for e in es:
+					count[f][e] += t[f][e] / total_s[f]
+					total[e] += t[f][e] / total_s[f]
 
-        for e in voce:
-            for f in vocf:
-                t[f][e] = count[f][e] / total[e]
-                if not t[f][e]:
-                    del t[f][e]
-        
-        log_pp = 0
-        for (fs, es) in pairs:
-            p = 0.0
-            for f in fs:
-                for e in es:
-                    p += t[f][e]
-            p *= (epsilon/(len(fs)**len(es)))
-            log_pp += np.log2(p)
-        old_perplexity = Decimal(perplexity)
-        perplexity = Context().power(2, Decimal(-log_pp))#2**(-perplexity)
-        stable = abs(old_perplexity - perplexity) < stability
-        print 'perplexity:', perplexity, ('stable' if stable else 'not stable')
-        
-    return t
-    
+		for e in voce:
+			for f in vocf:
+				t[f][e] = count[f][e] / total[e]
+				if not t[f][e]:
+					del t[f][e]
+
+		log_pp = 0
+		for (fs, es) in pairs:
+			p = 0.0
+			for f in fs:
+				for e in es:
+					p += t[f][e]
+			p *= (epsilon/(len(fs)**len(es)))
+			log_pp += np.log2(p)
+		old_perplexity = Decimal(perplexity)
+		# perplexity = Context().power(2, Decimal(-log_pp))#2**(-perplexity)
+		# stable = abs(old_perplexity - perplexity) < stability
+		print 'perplexity:', log_pp, ('stable' if stable else 'not stable')
+		i += 1
+
+	return t
+
 # Example uses:
 # round_dc(translate(pairs))
 t = translate([a for a in corpus(100)], 10000)
