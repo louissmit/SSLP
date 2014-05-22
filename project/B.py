8811235162283bac4a9bede9b75bcb0a8247b785
louissmit <ghr1Co74G9>
2014-05-22 02:37:07 +0200

    derp

--- project/B.py
+++ project/B.py
@@ -1,4 +1,5 @@
 
+import numpy as np
 
 class B:
 	def __init__(self):
@@ -6,3 +7,5 @@ class B:
 
 	def get(self, left_word, right_word):
 		print "not implemented yet"
+		# return np.random.random_sample()
+		return 213
--- project/localSearch.py
+++ project/localSearch.py
@@ -1,4 +1,5 @@
-
+from B import B
+import numpy as np
 
 def localSearch(B, sent):
 	"""
@@ -9,21 +10,36 @@ def localSearch(B, sent):
 	@return:
 	"""
 	n = len(sent)
-	beta = []
-	delta = []
-	for i in xrange(0, n-1):
-		beta[i] = []
-		beta[i][i+1] = 0
-		for k in xrange(i+1, n):
-			delta[i][i][k] = 0
-			delta[i][k][k] = 0
+	beta = np.zeros((n+1,n+1))
+	delta = np.zeros((n+1,n+1,n+1))
+	# for i in xrange(0, n):
+		# beta[i, i+1] = 0
+		# for k in xrange(i+1, n):
+		# 	delta[i, i, k] = 0
+		# 	delta[i, k, k] = 0
 
-	for w in xrange(2, n):
+	for w in xrange(2, n+1):
 		for i in xrange(0, n-w):
 			k = i + w
-			beta[i][k] = -100000000
+			beta[i, k] = float("-inf")
+			best_i = "not_assigned"
+			best_j = "not_assigned"
+			best_k = "not_assigned"
 			for j in xrange(i+1, k-1):
-				delta[i][j][k] = delta[i][j][k-1] + delta[i+1][j][k] - delta[i+1][j][k-1] + B.get(sent[k], sent[i+1]) - B.get(sent[i+1], sent[k])
-				beta[i][k] = max(beta[i][k], beta[i][j] + beta[j][k] + max(0, delta[i][j][k]))
+				delta[i, j, k] = delta[i, j, k-1] + delta[i+1, j, k] - delta[i+1, j, k-1] + B.get(sent[k], sent[i+1]) - B.get(sent[i+1], sent[k])
+				new_beta = beta[i, j] + beta[j, k] + max(0, delta[i, j, k])
+				print new_beta
+				if new_beta > beta[i, k]:
+					beta[i, k] = new_beta
+					best_i = i
+					best_j = j
+					best_k = k
+				print best_i, best_j, best_k
 
-	return beta[0][n]
+	print beta
+	return beta[0, n]
+
+
+if __name__ == '__main__':
+	b = B()
+	print localSearch(b, 'I am just testing'.split())
