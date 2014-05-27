e5c183ff97cd1b310608fe838813b5d932cf686c
louissmit <ghr1Co74G9>
2014-05-27 12:19:29 +0200

    local search done

--- project/B.py
+++ project/B.py
@@ -1,11 +1,30 @@
-
 import numpy as np
+from pprint import pprint
+
 
 class B:
-	def __init__(self):
-		self.matrix = []
+	def __init__(self, sent):
+		# alphabetic preference matrix for testing purposes
+		sorted_sent = []
+		for x in sorted(sent):
+			sorted_sent.append(x)
+		print sorted_sent
+		self.matrix = {}
+		for i, left_word in enumerate(sorted_sent):
+			if left_word not in self.matrix:
+				self.matrix[left_word] = {}
+			for j in xrange(i, len(sent)):
+				right_word = sorted_sent[j]
+				if right_word not in self.matrix:
+					self.matrix[right_word] = {}
+				if right_word not in self.matrix[left_word]:
+					self.matrix[left_word][right_word] = 1
+				if left_word not in self.matrix[right_word]:
+					self.matrix[right_word][left_word] = 0
+
+		# pprint(self.matrix)
+
 
 	def get(self, left_word, right_word):
-		print "not implemented yet"
-		return np.random.random_sample()
-		# return 213
+		# return np.random.random_sample()
+		return self.matrix[left_word][right_word]
--- project/localSearch.py
+++ project/localSearch.py
@@ -3,15 +3,18 @@ import numpy as np
 
 def localSearch(B, sent):
 	"""
-	@todo backpointers
+	Searches for the best local permutation
 
-	@param B:
-	@param sent:
-	@return:
+	@rtype : tuple
+	@param B: word pair preference matrix
+	@param sent: input sentence
+	@return: (delta, backpointers)
 	"""
 	n = len(sent)
 	beta = np.zeros((n+1,n+1))
+	bp = np.zeros((n+1,n+1))
 	delta = np.zeros((n+1,n+1,n+1))
+
 	for i in xrange(0, n):
 		beta[i, i+1] = 0
 		for k in xrange(i+1, n+1):
@@ -19,26 +22,40 @@ def localSearch(B, sent):
 			delta[i, k, k] = 0
 
 	for w in xrange(2, n+1):
-		for i in xrange(0, n-w):
+		for i in xrange(0, n-w+1):
 			k = i + w
 			beta[i, k] = float("-inf")
-			best_i = "not_assigned"
-			best_j = "not_assigned"
-			best_k = "not_assigned"
 			for j in xrange(i+1, k):
-				delta[i, j, k] = delta[i, j, k-1] + delta[i+1, j, k] - delta[i+1, j, k-1] + B.get(sent[k], sent[i+1]) - B.get(sent[i+1], sent[k])
+				delta[i, j, k] = delta[i, j, k-1] + delta[i+1, j, k] - delta[i+1, j, k-1] + B.get(sent[k-1], sent[i]) - B.get(sent[i], sent[k-1])
 				new_beta = beta[i, j] + beta[j, k] + max(0, delta[i, j, k])
 				if new_beta > beta[i, k]:
 					beta[i, k] = new_beta
-					best_i = i
-					best_j = j
-					best_k = k
-			print best_i, best_j, best_k
+					bp[i, k] = j
+	return delta, bp
 
-	print beta
-	return beta[0, n]
 
+def traverseBackpointers(sent, delta, bp, i, k):
+	"""
+	Recursively finds the permutation from the backpointers provided by localSearch
+
+	@param sent: input sentence
+	@param delta: swap probabilities
+	@param bp: backpointers
+	@param i: first index of range
+	@param k: last index of range
+	@return: permutation of input sentence
+	"""
+	j = int(bp[i, k])
+	if (k - i) > 1:
+		if delta[i, j, k] > 0:
+			return traverseBackpointers(sent, delta, bp, j, k) + traverseBackpointers(sent, delta, bp, i, j)
+		else:
+			return traverseBackpointers(sent, delta, bp, i, j) + traverseBackpointers(sent, delta, bp, j, k)
+	else:
+		return [sent[i]]
 
 if __name__ == '__main__':
-	b = B()
-	print localSearch(b, 'I am just testing'.split())
+	sent = 'beta alpha gamma zeta crap'.split()
+	b = B(sent)
+	delta, bp = localSearch(b, sent)
+	print traverseBackpointers(sent, delta, bp, 0, len(sent))
