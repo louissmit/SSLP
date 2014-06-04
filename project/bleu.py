455c0b77cfca4268ce6f61abc1d18ce7792141e3
louissmit <ghr1Co74G9>
2014-06-04 15:58:32 +0200

    go server

--- project/bleu.py
+++ project/bleu.py
@@ -2,11 +2,13 @@ def ngrams(s, n):
 	return [" ".join(s[i:i + n]) for i in range(len(s) - n + 1)]
 
 def precision(gold, out):
-	return float(float(len(set(gold)) & set(out))) / float(float(len(set(out))))
+	print gold
+	print out
+	return float(len(set(gold) & set(out))) / float(len(set(out)))
 
 
 def bleu(gold, out):
-	bp = min(1, len(out) / len(gold))
+	bp = min(1, float(len(out)) / float(len(gold)))
 	ps = [precision(ngrams(gold, n), ngrams(out, n)) for n in [1, 2, 3, 4]]
 	print ps
 	return bp * ps[0] * ps[1] * ps[2] * ps[3]
