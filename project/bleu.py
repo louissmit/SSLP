9ad1651ced56225f3c4c20223a20b978873f50e3
Benno Kruit <bennokr@gmail.com>
2014-06-06 13:30:32 +0200

    bleu fix

--- project/bleu.py
+++ project/bleu.py
@@ -2,11 +2,12 @@ def ngrams(s, n):
 	return [" ".join(s[i:i + n]) for i in range(len(s) - n + 1)]
 
 def precision(gold, out):
+	if not gold or not out:
+		return 1
 	return float(len(set(gold) & set(out))) / float(len(set(out)))
 
 
 def bleu(gold, out):
 	bp = min(1, float(len(out)) / float(len(gold)))
 	ps = [precision(ngrams(gold, n), ngrams(out, n)) for n in [1, 2, 3, 4]]
-	print ps
 	return bp * ps[0] * ps[1] * ps[2] * ps[3]
